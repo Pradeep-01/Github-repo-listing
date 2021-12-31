3cc78b1e899b581671bfa17be566f52b4a3b8d52
Pradeep 01 <pradeeptheunique007@gmail.com>
2021-12-31 16:56:52 +0530

    add basename and declared /app

--- src/component/Mainpage.js
+++ src/component/Mainpage.js
@@ -8,7 +8,7 @@ const Mainpage = () => {
   
 return (
   <div style={{margin:"5px", boxSizing:"border-box"}}>
-    <Router>
+    <Router basename="/app">
     <Routes>
       <Route exact path="/" element={<Maincard />}/>
       <Route path="/:id" element={<Program />}/>
