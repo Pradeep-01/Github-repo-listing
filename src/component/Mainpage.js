0cbeeb97f0c9fd64ab06bf060c73887db01021bd
Pradeep 01 <pradeeptheunique007@gmail.com>
2022-01-23 21:01:07 +0530

    used hashrouter instead of browser router

--- src/component/Mainpage.js
+++ src/component/Mainpage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, HashRouter } from "react-router-dom";
 import Maincard from './Maincard';
 import Program from './Program';
 
@@ -8,12 +8,12 @@ const Mainpage = () => {
   
 return (
   <div style={{margin:"5px", boxSizing:"border-box"}}>
-    <Router basename={process.env.PUBLIC_URL}>
+    <HashRouter >
     <Routes>
       <Route exact path="/" element={<Maincard />}/>
       <Route path="/:id" element={<Program />}/>
     </Routes>
-  </Router>
+  </HashRouter>
   </div>
     
 )
