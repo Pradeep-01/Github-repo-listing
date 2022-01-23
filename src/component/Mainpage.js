ae797b60cdbadec25a4cf54c310b7d8a40c0d5b5
Pradeep 01 <pradeeptheunique007@gmail.com>
2022-01-23 23:59:12 +0530

    removed basename . only hashrouter

--- src/component/Mainpage.js
+++ src/component/Mainpage.js
@@ -8,7 +8,7 @@ const Mainpage = () => {
   
 return (
   <div style={{margin:"5px", boxSizing:"border-box"}}>
-    <HashRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter > {/* <Router basename={process.env.PUBLIC_URL}/> */}
     <Routes>
       <Route exact path="/" element={<Maincard />}/>
       <Route path="/:id" element={<Program />}/>
